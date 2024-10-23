d41a9b2ea683fdf0f69bb9e55a35c53b14c62989
yooju00 <yooju00@naver.com>
2024-10-23 19:19:12 +0900

    feat(App): 게임 실행 흐름 제어 기능 구현
    - 게임 초기화 및 실행 로직 구현
    - 예외 처리 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { Console } from "@woowacourse/mission-utils";
+import InputView from "./view/InputView.js";
+import OutputView from "./view/OutputView.js";
+import Game from "./domain/Game.js";
+
 class App {
-  async run() {}
+  #inputView;
+  #outputView;
+
+  constructor() {
+    this.#inputView = new InputView();
+    this.#outputView = new OutputView();
+  }
+
+  async run() {
+    try {
+      const carNames = await this.#inputView.readCarNames();
+      const attempts = await this.#inputView.readAttempts();
+
+      const game = new Game(carNames, attempts);
+
+      this.#outputView.printGameStart();
+
+      for (let i = 0; i < attempts; i++) {
+        await game.play();
+        this.#outputView.printRoundResult(game.getCurrentPositions());
+      }
+
+      this.#outputView.printWinners(game.getWinners());
+    } catch (error) {
+      Console.print(error.message);
+      throw error;
+    }
+  }
 }
 
 export default App;
